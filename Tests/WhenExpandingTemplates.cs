8039d0db15688e2fa98ed99294ef7c6913f95881
Leon Zhou <leon.zhou@smokeball.com>
2022-03-23 08:39:01 +1100

    Added unit tests for template expander and secret santa.

---
+++ Tests/WhenExpandingTemplates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Core;
+using NUnit.Framework;
+
+namespace Tests
+{
+    internal class WhenExpandingTemplates
+    {
+        [Test]
+        public void ShouldPopulateTemplate()
+        {
+            var dateTime = new DateTime(2000,1,1);
+            var @enum = DateTimeKind.Local;
+            Assert.AreEqual(
+                TemplateExpander.PopulateTemplate(
+                    "Lorem ipsum {key1} sit amet, conse{key2}ctetur adipiscing {key3}, sed do {key4}{key5} ut labore et.",
+                    new Dictionary<string, object>
+                    {
+                        {"key1", 11},
+                        {"key2", "cillum"},
+                        {"key3", dateTime},
+                        {"key4", @enum},
+                        {"key5", 'c'}
+                    }),
+                $"Lorem ipsum {11} sit amet, conse{"cillum"}ctetur adipiscing {dateTime}, sed do {@enum}{'c'} ut labore et.");
+        }
+    }
+}
---
+++ Tests/WhenGeneratingSecretSantaList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Core;
+using NUnit.Framework;
+
+namespace Tests
+{
+    internal class WhenGeneratingSecretSantaList
+    {
+        [Test]
+        public void ShouldGenerateEmptyList()
+        {
+            Assert.IsEmpty(SecretSantaManager.GenerateSecretSantaList(Array.Empty<string>()));
+            Assert.IsEmpty(SecretSantaManager.GenerateSecretSantaList(new[] {"name"}));
+        }
+
+        [Test]
+        public void ShouldGenerateSimpleSantaList()
+        {
+            Assert.IsTrue(new[] {("name 1", "name 2"), ("name 2", "name 1")}
+                .SequenceEqual(SecretSantaManager.GenerateSecretSantaList(new[] {"name 1", "name 2"}).OrderBy(x => x)));
+        }
+
+        [Test]
+        public void ShouldGenerateSecretSantaList()
+        {
+            // Generate a random list of 5 to 10 names.
+            var random = new Random();
+            var names = Enumerable.Range(0, random.Next(5, 10))
+                .Select(_ => new string(Enumerable.Range(0, random.Next(5, 10))
+                    .Select(__ => Convert.ToChar(random.Next(97, 122)))
+                    .ToArray()))
+                .ToList();
+
+            // Test 1: the generated secret Santa messages must be able to convert to a dictionary,
+            //         because there can't be any duplicate senders.
+            var secretSantaMessages = SecretSantaManager.GenerateSecretSantaList(names)
+                .ToDictionary(x => x.Sender, x => x.Receiver);
+
+            // Test 2: the generated secret Santa messages must not have any duplicate receivers.
+            Assert.AreEqual(secretSantaMessages.Values.Distinct().Count(), secretSantaMessages.Values.Count);
+
+            // Test 3: the number of senders must be the same as the number of names.
+            Assert.AreEqual(secretSantaMessages.Keys.Count, names.Count);
+
+            // Test 4: the number of receivers must be the same as the number of names.
+            Assert.AreEqual(secretSantaMessages.Values.Count, names.Count);
+        }
+    }
+}
